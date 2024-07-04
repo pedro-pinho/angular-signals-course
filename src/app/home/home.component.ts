b09a2865d0ea5186ddfd070574d010ca32e7e20d
Pedro dos Santos Pinho <pedro.dossantos@inbrace.com>
2024-07-04 10:55:40 -0300

    chore: learning computed signals

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,6 @@ import {MatDialog} from "@angular/material/dialog";
 import {MessagesService} from "../messages/messages.service";
 import {catchError, from, throwError} from "rxjs";
 import {toObservable, toSignal, outputToObservable, outputFromObservable} from "@angular/core/rxjs-interop";
-type Counter = {
-  value: number;
-};
 @Component({
   selector: 'home',
   standalone: true,
@@ -23,19 +20,19 @@ type Counter = {
 })
 export class HomeComponent {
   //Advantages of using signals: Improved change detection, better performance, and more predictable behavior.
-  counter = signal<Counter>({
-    value: 0
+  counter = signal(0);
+
+  tenXCounter = computed(() => {
+    const counterValue = this.counter();
+    return counterValue * 10;
   });
-  values = signal<number[]>([0]);
+
+  hundredXCounter = computed(() => {
+    const counterValue = this.tenXCounter();
+    return counterValue * 10;
+  });
 
   increment(): void {
-    this.counter.update(counter => ({
-      ...counter,
-      value: counter.value + 1
-    }));
-  }
-
-  appendValue(): void {
-    this.values.update(values => [...values, values.length]);
+    this.counter.update(val => (val + 1));
   }
 }
